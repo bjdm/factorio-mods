e1a303d7bf0cc9297277fb4de18716f3dec3fd78
Brent Mackey <brent.mackey@connect.qut.edu.au>
2017-02-01 23:36:50 +1000

    try now

--- longer-underground-pipes_0.14.0/factorio-longer-underground-belts_0.14.0/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-path="longer-underground-belts_0.14.0"
-
-mkdir -p "build/$path"
-cp -r data.lua info.json "build/$path/"
-cd build
-zip -r --compression-method=deflate "$path.zip" "$path"
